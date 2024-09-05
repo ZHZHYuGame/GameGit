80d4c792cae38c83a4e603448cd0fcd10f243224
lhzec <3201985609@qq.com>
2024-09-05 21:38:13 +0800

    交作业

---
+++ 张恩慈/9.5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2,5, 23, 6 3, 4, 6, 40;
+            Console.WriteLine("--------------------原数组--------------------");
+            foreach (int a in array)
+            {
+                Console.Write(a + ",");
+            }
+            Console.WriteLine();
+            InsertSort(array);
+            Console.WriteLine("--------------------插入排序--------------------");
+            foreach (int a in array)
+            {
+                Console.Write(a + ",");
+            }
+            Console.WriteLine();
+        }
+
+        private static void InsertSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int insertVal = array[i];
+                int insertIndex = i - 1;
+                while (insertIndex >= 0 && insertVal < array[insertIndex])
+                {
+                    array[insertIndex + 1] = array[insertIndex];
+                    insertIndex--;
+                }
+                array[insertIndex + 1] = insertVal;
+            }
+        }
+    }
+}
