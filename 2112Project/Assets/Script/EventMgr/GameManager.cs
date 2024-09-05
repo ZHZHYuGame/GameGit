4611c6da2222bde6c406b5b8d7b33ab12d405c42
Super_Ikun <2215698024@qq.com>
2024-09-05 17:27:17 +0800

    1

--- 2112Project/Assets/Script/EventMgr/GameManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Search;
-using UnityEngine;
-
-public class GameManager : MonoBehaviour
-{
-    public static MessageEventMgr eventMgr = new MessageEventMgr();
-}
