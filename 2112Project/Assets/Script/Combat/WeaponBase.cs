23c80d528735f154d0c7ab1816cd08cac987b268
song <1213467817@qq.com>
2024-09-06 08:39:02 +0800

    .

---
+++ 2112Project/Assets/Script/Combat/WeaponBase.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBase:MonoBehaviour
+{
+    public float atk;
+    public GameObject Player;
+   protected virtual void Start() { }
+
+    protected virtual void Update() { }
+}
