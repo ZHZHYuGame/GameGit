75cc7fb44357a8ebe6c7f68a65b89908aefd4d8d
Zheng_g <2970782247@qq.com>
2024-09-11 16:52:06 +0800

    添加多语言配置文件 添加多语言使用说明 添加LanguageInfo文件 -- 存储语言Key常量

---
+++ 2112Project/Assets/Script/Language/LanguageInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageInfo 
+{
+    public const string Start_Game_TEXT = "开始游戏";
+    public const string End_Game_TEXT = "结束游戏";
+}
