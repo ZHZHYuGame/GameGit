780b67bb1257fd7bf32515dac619e0fc6191b0e0
LiTao <252988727@qq.com>
2024-09-13 16:16:46 +0800

    李涛提交系统设置

---
+++ 2112Project/Assets/SystemSettting/GMCommandSetting.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// GM√¸¡Ó
+/// </summary>
+public class GMCommandSetting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
